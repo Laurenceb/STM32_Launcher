17534632fe52cc8270ca4bffab7b1e1e13c33418
Laurence Blaxter <Laurence.blaxter@btinternet.com>
2015-02-21 14:59:05 +0000

    Bug fixes for shutdown logic

--- main.c
+++ main.c
@@ -318,11 +318,8 @@ int main(void)
 				CUTDOWN;
 				cutofftime=Millis+6000;	//Cutter on for 6 seconds
 			}
-			else if(system_state==2 && (shutdown_lock!=SHUTDOWNLOCK_MAGIC)) {//Treble press turns off
+			else if(system_state==3 ) 	//Treble press turns off
 				Shutdown_System=MULTIPRESS_TURNOFF;
-				shutdown_filesystem(Shutdown_System, file_opened);
-				shutdown();
-			}
 			System_state_Global&=~0x80;	//Wipe the flag bit to show this has been processed
 		}
 		if(Millis>cutofftime && cutofftime) {	//Reset the cutdown later
@@ -360,8 +357,8 @@ int main(void)
 			last_message=Millis;
 		}
 		}//Context only
-                if(Shutdown_System) {			//A system shutdown has been requested - this allows the board to be turned off by pressing during GPS lock
-			if(file_opened)
+                if((Shutdown_System&&(shutdown_lock!=SHUTDOWNLOCK_MAGIC)) || (Shutdown_System==MULTIPRESS_TURNOFF)) {//A system shutdown has been requested 
+			if(file_opened)			//this allows the board to be turned off by pressing during GPS lock
 				shutdown_filesystem(Shutdown_System, file_opened);
 			if(Shutdown_System==USB_INSERTED)
 				NVIC_SystemReset();	//Software reset of the system - USB inserted whilst running
@@ -548,7 +545,7 @@ int main(void)
 		//Deal with file size - may need to preallocate some more
 		file_preallocation_control(&FATFS_logfile);
 		file_preallocation_control(&FATFS_wavfile_gyro);
-                if((Shutdown_System&&(shutdown_lock!=SHUTDOWNLOCK_MAGIC))||Shutdown_System==MULTIPRESS_TURNOFF) {//A system shutdown has been requested
+                if((Shutdown_System&&(shutdown_lock!=SHUTDOWNLOCK_MAGIC))||(Shutdown_System==MULTIPRESS_TURNOFF)) {//A system shutdown has been requested
 			if(file_opened)
 				shutdown_filesystem(Shutdown_System, file_opened);
 			if(Shutdown_System==USB_INSERTED)
